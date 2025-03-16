89425e0912d4214b328bac0988b5a0a3e4ff1803
AndresG191 <andresrgomez191@gmail.com>
2025-03-15 23:29:22 -0600

    up

--- Implementacion_KMEANS.py
+++ Implementacion_KMEANS.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.io as pio
@@ -12,37 +13,37 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# Configuración para mostrar gráficos en el navegador.
+#Configuración para mostrar gráficos en el navegador.
 pio.renderers.default = 'browser'
 
 df = pd.read_csv("numeros_aleatorios.csv")
 print(df)
 
-# Normalización de los datos
+#Normalización de los datos
 X = df.copy()
 X1 = preprocessing.normalize(X)
 
-# Visualización del codo para elegir el número de clusters
+#Visualización del codo para elegir el número de clusters
 model = KMeans()
 visualizer = KElbowVisualizer(model, k=(1, 10))
 visualizer.fit(X1)  # Entrenamos los datos
 visualizer.show()  # Renderizamos la imagen del codo
 
-# Algoritmo KMeans para crear los clusters
+#Algoritmo KMeans para crear los clusters
 algorithm = KMeans(n_clusters=5, init='k-means++', n_init=10,
                    max_iter=300, tol=0.0001, random_state=111)
 algorithm.fit(X1)
 labels = algorithm.labels_
-centroids = algorithm.cluster_centers_ #Los centroides son obtenidos de aqui
+centroids = algorithm.cluster_centers_
 
-# Añadir las etiquetas al DataFrame
+#Añadir las etiquetas al DataFrame
 df['label'] = labels
 
 #Obtiene el grafico en 2d
 pca = PCA(n_components=2)
 pca_result = pca.fit_transform(X1)
 
-# Asignar las componentes principales a las columnas X1, X2
+#Asignar las componentes principales a las columnas X1, X2
 df['X1'] = pca_result[:, 0]
 df['X2'] = pca_result[:, 1]
 
@@ -57,6 +58,15 @@ fig.add_scatter(x= centroids_pca[:,0], y=centroids_pca[:,1],
                 mode ='markers', marker=dict(color='red', size=8), name='Centroides')
 fig.show()
 
-# Guardar el gráfico en un archivo HTML
 fig.write_html("file.html")
 
+#Paso para calcular las distancias de cada INSTANCIA y los centroides
+
+#Matriz de distancias
+distancias = np.zeros((X1.shape[0], centroids.shape[0]))
+
+for i in range(X1.shape[0]):
+    for j in range(centroids.shape[0]):
+        distancias[i, j] = np.linalg.norm(X1[i] - centroids[j])
+print("La matriz de distancias entre las instancias y los centroides son las siguientes: ")
+print(distancias)
